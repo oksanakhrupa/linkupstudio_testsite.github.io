aa2876537833cfae63705e000752c6b357ced296
Oksana Khrupa <khrupaoksana@gmail.com>
2018-12-10 14:13:40 +0200

    first website upload

---
+++ assets/js/slider.js
@@ -0,0 +1,9 @@
+    $(document).on('ready', function() {
+      $(".center").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+      });
+    });
